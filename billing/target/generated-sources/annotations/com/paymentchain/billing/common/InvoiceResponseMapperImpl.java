aaf8f504e318d636e11e47b6039a1f1f2d3a8d82
AlejandraCalderon254 <mariaalejandra.calderonquevedo@amaris.com>
2025-04-09 06:23:44 -0500

    add integrating test junit

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResponseMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResponseMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-02T05:16:13-0500",
+    date = "2025-04-09T05:41:49-0500",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
